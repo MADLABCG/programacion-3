cf052135584e91969fb2ac9f527ee98f7cdac790
Ivan Firestone <ivan.firestone@icloud.com>
2024-10-27 10:09:34 -0400

    Proyecto Final

---
+++ 2024-03/proyectofinal/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Información</title>
+    <!-- <link rel="stylesheet" href="/css/styles.css"> -->
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+</head>
+
+<body>
+    <form class="well">
+        <label>Label name</label>
+        <input type="text" class="span3" placeholder="Type something…">
+        <span class="help-inline">Associated help text!</span>
+        <label class="checkbox">
+            <input type="checkbox"> Check me out
+        </label>
+        <button type="submit" class="btn">Submit</button>
+    </form>
+
+    <div>
+        <a class="btn" href="">Link</a>
+        <button class="btn" type="submit">
+            Button
+        </button>
+        <input class="btn" type="button" value="Input">
+        <input class="btn" type="submit" value="Submit">
+    </div>
+    <br>
+    <br>
+    <div>
+        <button class="btn">Por Defecto</button>
+        <button class="btn btn-primary">Primario</button>
+        <button class="btn btn-info">Info</button>
+        <button class="btn btn-success">Exitoso</button>
+        <button class="btn btn-warning">Advertencia</button>
+        <button class="btn btn-danger">Peligro</button>
+        <button class="btn btn-inverse">Invertido</button>
+    </div>
+
+</body>
+
+</html>
